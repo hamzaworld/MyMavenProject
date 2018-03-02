81e44e4a6f33df4da75c51638fe247bbb9b4816d
Hamza Akhtar <hamza019akhtar@gmail.com>
2018-03-02 16:08:56 +0530

    second commit

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -1,9 +1,9 @@
 package com.demoproject;
 class HelloWorld{
 
-public sttaic void main(String args[]){
+public static void main(String args[]){
 
-System.out.println("This is sample hello wolrd program!");
+System.out.println("This is sample hello wolrd 1213  program!");
 
 }
 }
